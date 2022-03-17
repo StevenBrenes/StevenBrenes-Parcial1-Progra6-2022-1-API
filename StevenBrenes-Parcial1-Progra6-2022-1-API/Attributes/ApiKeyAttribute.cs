31a1d63c782e4af4050434c5d640e2236d8db27f
Steven Brenes <sbgdp@outlook.com>
2022-03-16 21:53:52 -0600

    API finalizado

---
+++ StevenBrenes-Parcial1-Progra6-2022-1-API/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace StevenBrenes_Parcial1_Progra6_2022_1_API.Attributes
+{
+    [AttributeUsage(validOn: AttributeTargets.All)]
+    public sealed class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+
+        private const string NombreDelApiKey = "ApiKey";
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(NombreDelApiKey, out var ApiSalida))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "No se ha incluido una API Key."
+                };
+
+                return;
+
+
+            }
+
+            var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+
+            var apikey = appSettings.GetValue<string>(NombreDelApiKey);
+
+            if (!apikey.Equals(ApiSalida))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "La API Key suministrada no es la correcta."
+                };
+
+                return;
+
+            }
+
+            await next();
+
+        }
+
+    }
+}
