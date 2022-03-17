a58c0d8bafe3001441e82cbaa6170953cb2472d0
Steven Brenes <sbgdp@outlook.com>
2022-03-16 19:50:20 -0600

    Agregar archivos de proyecto.

---
+++ StevenBrenes-Parcial1-Progra6-2022-1-API/Models/P6Assets20221Context.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace StevenBrenes_Parcial1_Progra6_2022_1_API.Models
+{
+    public partial class P6Assets20221Context : DbContext
+    {
+        public P6Assets20221Context()
+        {
+        }
+
+        public P6Assets20221Context(DbContextOptions<P6Assets20221Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Activo> Activos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("SERVER=192.168.1.111; DATABASE=P6Assets20221; User Id=sa; Password=HacHxnZ6");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Activo>(entity =>
+            {
+                entity.HasKey(e => e.Idactivo)
+                    .HasName("PK__Activo__4D26804413838435");
+
+                entity.ToTable("Activo");
+
+                entity.Property(e => e.Idactivo).HasColumnName("IDActivo");
+
+                entity.Property(e => e.CostoActivo).HasColumnType("decimal(19, 2)");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(1000)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EstadoActivo)
+                    .IsRequired()
+                    .HasDefaultValueSql("('1')");
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("date")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.NombreActivo)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NumeroSerie)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Responsable)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Ubicacion)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ StevenBrenes-Parcial1-Progra6-2022-1-API/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StevenBrenes_Parcial1_Progra6_2022_1_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace StevenBrenes_Parcial1_Progra6_2022_1_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+            services.AddControllers();
+          
+
+            var conn = @"SERVER = 192.168.1.111; DATABASE = P6Assets20221; User Id = sa; Password = HacHxnZ6";
+
+            services.AddDbContext<P6Assets20221Context>(options => options.UseSqlServer(conn));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "StevenBrenes_Parcial1_Progra6_2022_1_API", Version = "v1" });
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StevenBrenes_Parcial1_Progra6_2022_1_API v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
